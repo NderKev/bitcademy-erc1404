ab109e0c7a9c445ea649d987a5d8644a3d1a4e15
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2019-05-01 00:44:27 +0200

    Main contracts migrations

---
+++ migrations/2_deploy_all_contracts.js
@@ -0,0 +1,20 @@
+var RegulatedTokenERC1404 = artifacts.require("./RegulatedTokenERC1404.sol");
+var RegulatorService = artifacts.require("./RegulatorService.sol");
+var ServiceRegistry = artifacts.require("./ServiceRegistry.sol");
+
+module.exports = async function(deployer, network, accounts) {
+  let name = "Bitcademy Gold"
+  let symbol = "BTMG"
+  let regulatorService, serviceRegistry;
+
+  await deployer.deploy(RegulatorService, {gas: 6000000});
+  regulatorService = await RegulatorService.deployed();
+  await deployer.deploy(ServiceRegistry, regulatorService.address, {gas: 6000000});
+  serviceRegistry = await ServiceRegistry.deployed();
+  await deployer.deploy(
+    RegulatedTokenERC1404,
+    serviceRegistry.address,
+    name,
+    symbol,
+    {gas: 6000000});
+};
